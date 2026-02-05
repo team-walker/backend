8c738d142db75bd2073f7ce58b8bc0275bdb89d0
YOJIN003 <yammi1220@naver.com>
2026-02-05 17:45:00 +0900

    refactor: remove AuthGuard from exports in AuthModule #22

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,6 +6,7 @@ import { AuthGuard } from './auth.guard';
 @Module({
   controllers: [AuthController],
   providers: [AuthGuard],
-  exports: [AuthGuard],
+  // 다른 모듈에서 직접 주입(Injection)할 일이 없으므로 exports는 비워둡니다.
+  exports: [],
 })
 export class AuthModule {}
